c6174d98b669259de039c99e1bf1c6277fb18731
Ilya Egorychev <ilegorro@gmail.com>
2021-06-08 15:14:34 +0300

    Set ext list

--- .eslintrc.js
+++ .eslintrc.js
@@ -3,7 +3,7 @@ module.exports = {
   env: {
     node: true,
   },
-  extends: ['plugin:vue/vue3-essential', 'prettier'],
+  extends: ['plugin:vue/vue3-essential', 'eslint:recommended', '@vue/prettier'],
   parserOptions: {
     ecmaVersion: 2020,
   },
